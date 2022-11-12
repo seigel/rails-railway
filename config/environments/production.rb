ef4b29a6b50dd4c6433c1f1437df99ed8b5b5311
James Seigel <github@paddlethis.com>
2022-11-11 21:26:29 -0700

    adding crazy host for railway deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.hosts << "rails-production-f1c0.up.railway.app"
+
 end

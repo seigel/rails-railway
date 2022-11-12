d893a276a0ade9f1616b5cf47490c42392899435
James Seigel <github@paddlethis.com>
2022-11-11 21:42:33 -0700

    Make it more open maybe?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.hosts << "rails-production-f1c0.up.railway.app"
+  config.hosts << ".railway.app"
+
 
 end
